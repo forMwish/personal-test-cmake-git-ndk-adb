538e8d6d86421a56b92aef58be99908ee801aa2f
muziwenwu <li296641798@gmail.com>
2020-02-02 16:49:26 +0800

    cmake init

--- source/test.cpp
+++ source/test.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "CmakeConfig.h"
 
 int main(int argc, char** argv)
 {
-    std::cout << "hello world" << std::endl;
-    return 0;
+  if (argc < 2) {
+    // report version
+    std::cout << argv[0] << " Version " << Test_VERSION_MAJOR << "."
+              << Test_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
 }
