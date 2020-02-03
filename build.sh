5579bf00947c7d32e6d7e18cff9a0023d4b9fe6e
muziwenwu <li296641798@gmail.com>
2020-02-03 22:53:12 +0800

    update build.sh: add cross-compile

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+NDK="/opt/android-ndk-r20b"
+ANDROID_ABI="armeabi-v7a"
+ANDROID_ARM_MODE="thumb"
+ANDROID_ARM_NEON="TRUE"
+# ANDROID_LD="lld" #选择链接器
+# ANDROID_PLATFORM="" #同 ANDROID_NATIVE_API_LEVEL 最低API级别，通常有 NDK 自动填入
+ANDROID_STL="c++_static"
+# echo $ANDROID_ABI
 rm -r ./build/*
 cd ./build  &&
-cmake ..    &&
+# cmake ..    &&
+cmake .. \
+        -DCMAKE_TOOLCHAIN_FILE=$NDK/build/cmake/android.toolchain.cmake \
+        -DANDROID_ABI=$ANDROID_ABI \
+        -DANDROID_ARM_MODE=$ANDROID_ARM_MODE \
+        -DANDROID_ARM_NEON=$ANDROID_ARM_NEON \
+            &&
 cmake --build .
